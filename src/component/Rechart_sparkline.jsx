a3be10bd6f37785792509e579efde8e5fd269a84
blair <blairzheng0312@gmail.com>
2023-03-19 21:28:50 +1100

    updated math function

--- src/component/Rechart_sparkline.jsx
+++ src/component/Rechart_sparkline.jsx
@@ -1,59 +1,51 @@
 import React, { PureComponent } from 'react';
 import { LineChart, Line, ResponsiveContainer } from 'recharts';
+import {ave} from "./ave"
+
 const data = [
   {
     name: 'Page A',
-    uv: 4000,
+    y: 4000,
   },
   {
     name: 'Page B',
-    uv: 3000,
+    y: 3000,
 
   },
   {
     name: 'Page C',
-    uv: 2000,
+    y: 2000,
   },
   {
     name: 'Page D',
-    uv: 2780,
+    y: 2780,
   },
   {
     name: 'Page E',
-    uv: 1890,
+    y: 1890,
   },
   {
     name: 'Page F',
-    uv: 2390,
+    y: 2390,
   },
   {
     name: 'Page G',
-    uv: 3490,
+    y: 3490,
   },
 ];
-
-function amount(item){
-  return item.uv;
-}
-function sum(prev, next){
-  return prev + next;
-}
-data.map(amount).reduce(sum);
-let ave = data.map(item => item.uv).reduce((prev, next) => prev + next);
 
 const renderLineChart = (
   <ResponsiveContainer width={300} height={100}>
     <LineChart width="50%" height="50%" data={data} margin={{ left: 160 }}>
-      <Line type="linear" dataKey="uv" stroke="#444" dot={false} strokeWidth={3}
+      <Line type="linear" dataKey="y" stroke="#444" dot={false} strokeWidth={3}
       />
       <text
         x='40%'
         y='40%'
         style={{ fontSize: 24, fontWeight: 'bold', fill: '#444' }}
-        width="100%"
-        
+        width="100%"       
       >
-        {Math.round(ave/data.length, 0)}
+        {ave(data)}
       </text>
       <text
         x='40%'
